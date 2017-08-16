b3fa5d4353d864ff1fad0ad5e866bc3c58fcbe6b
Eswari1421104 <eswaribaskar24@gmail.com>
2017-08-16 21:54:27 +0530

    Create Camelcase.java

---
+++ Camelcase.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Camelcase
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        String res="";
+        char f=str.charAt(0);
+        res=res+Character.toUpperCase(f);
+        for(int i=1;i<str.length();i++)
+        {
+           char curchar=str.charAt(i);
+          char  prechar=str.charAt(i-1);
+            if(prechar==' ')
+            {
+                res=res+Character.toUpperCase(curchar);
+            }
+            else
+            {
+                res=res+curchar;
+            }
+        }
+        System.out.println(res);
+        
+    }
+}
